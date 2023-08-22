fbc6a3a8ed5542c8544efd5cbe3c17a3f99db803
Юлия Нелюбина <loginovskaya.yuliya.677@mail.ru>
2023-08-23 02:29:49 +0900

    созданы базовые модели, урлы, сериализаторы

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from reviews.models import Title, Genre, Category
+from .serializers import TitleSerializer, GenreSerializer, CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    '''Вьюсет для модели Категории. Читать может любой пользователь'''
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    '''Вьюсет для модели Жанры. Читать может любой пользователь'''
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    '''Вьюсет для Произведений. Читать может любой пользователь'''
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Category, Genre, Title
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    search_fields = ('name',)
+    empty_value_diplay = '-пусто-'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    search_fields = ('name',)
+    empty_value_diplay = '-пусто-'
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'year', 'category')
+    search_fields = ('name',)
+    list_filter = ('year', 'category')
+    empty_value_diplay = '-пусто-'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
