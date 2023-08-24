e4456b1f5c271278879f184698f7e3e6975ee43a
Александр Дубровин <insidder@yandex.ru>
2023-08-24 17:06:09 +0700

    models/views/serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Title, Genre, Category
+from reviews.models import Title, Genre, Category, Review, Comment
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -22,3 +22,59 @@ class TitleSerializer(serializers.ModelSerializer):
             'id', 'name', 'category', 'genre',
             'description', 'year',
         )
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания отзывов"""
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True,
+    )
+    score = serializers.IntegerField(
+        min_value=1, max_value=10
+    )
+
+    class Meta:
+        model = Review
+        fields = (
+            'id', 'text', 'author', 'score', 'pub_date'
+        )
+        read_only = ('id',)
+
+    def validate(self, data):
+        request = self.context.get('request')
+        title = self.context['view'].kwargs.get('title_id')
+        if Review.objects.filter(author=request.user, title=title).exists():
+            raise serializers.ValidationError('У вас уже есть отзыв'
+                                              'на это произведение')
+        return data
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор для работ с отзывами"""
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True,
+    )
+    score = serializers.IntegerField(
+        min_value=1, max_value=10
+    )
+
+    class Meta:
+        model = Review
+        fields = (
+            'id', 'text', 'author', 'score', 'pub_date'
+        )
+        read_only = ('id',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для комментариев"""
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = (
+            'id', 'text', 'author', 'pub_date'
+        )
+        read_only = ('id',)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
-MODERATOR = 'moderator'
-ADMIN = 'admin'
-USER = 'user'
-
-
-ROLES = [
-    (MODERATOR, 'Модератор'),
-    (ADMIN, 'Администратор'),
-    (USER, 'Пользователь')   
-]
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Category(models.Model):
@@ -95,6 +84,15 @@ class Title(models.Model):
 
 class User(AbstractUser):
     '''Переопределяем модель User'''
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    USER = 'user'
+
+    ROLES = [
+        (MODERATOR, 'Модератор'),
+        (ADMIN, 'Администратор'),
+        (USER, 'Пользователь'),
+    ]
     username = models.CharField(
         max_length=150,
         verbose_name='Пользователь',
@@ -128,10 +126,81 @@ class User(AbstractUser):
         max_length=100,
         verbose_name='Роль',
         choices=ROLES,
-        default='user'
+        default=USER
     )
 
     class Meta:
         ordering = ('username',)
         verbose_name = 'Пользователь',
         verbose_name_plural = 'Пользователи'
+
+
+class Review(models.Model):
+    """Модель отзывов на произведения"""
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва'
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Баллы отзыва',
+        validators=[MinValueValidator(1, 'Наинизшая балл'),
+                    MaxValueValidator(10, 'Наивысший балл')]
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author'
+            )
+        ]
+        ordering = ['-pub_date']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    """Модель комментариев к отзывам"""
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Комментарий'
+    )
+    text = models.TextField(
+        verbose_name='Текст комментария'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
